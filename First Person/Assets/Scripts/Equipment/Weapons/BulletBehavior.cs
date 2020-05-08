007e413a963b808b6ad46f2c5b06f55a89a295cb
Lucas Freitas <lucasfreitas09876@gmail.com>
2020-05-07 22:01:07 -0400

    Starting set up for bullet behavior
    - Set up inputs to recieve FPSCamera data
    - Set up LineRenderer for tracer GFX
    - Plans for gravity effected bullets

--- First Person/Assets/Scripts/Equipment/Weapons/BulletBehavior.cs
+++ First Person/Assets/Scripts/Equipment/Weapons/BulletBehavior.cs
@@ -4,28 +4,53 @@ using UnityEngine;
 
 public class BulletBehavior: MonoBehaviour
 {
-    private Vector3 dir = Vector3.forward;
+    private LineRenderer line;
+    private Vector3[] lineVertices = new Vector3[2];
+
+    private Ray ray;
+    private Vector3 dir;
     private RaycastHit hit;
 
     private float lifeTime = 0.2f;
+    private float magnitude;
 
-    public Vector3 Direction { set => dir = value;  }
 
-    void Awake()
-    {
-        if(Physics.Raycast(transform.position, dir, out hit)) {
-            //If it hit something, check if it's damageble. 
-            //yes? deal damage.
-            //no? get texture coordinate and apply hit texture.
-        } else {
-            //destroy self after x time
-        }
 
-        Debug.Log("Pew!");
+    
+    
+
+    public Vector3 Direction { set => dir = value; }
+    public float Magnitude { set => magnitude = value; }
+
+
+
+    void Awake(){
+        //Set variables
+        line = GetComponent<LineRenderer>();
+        ray = new Ray(transform.position, dir);
+
+        //shoot
+        ShootRay();// this will start a chain reaction of rays later on
+        //draw
+        RenderTracer();
+
     }
 
     void Update() {
         Timer(lifeTime);
+    }
+
+    void ShootRay() {
+        if(Physics.Raycast(ray, out hit, magnitude)) {
+            //If it hit something, check if it's damageble. 
+            //yes? deal damage.
+            //no? get texture coordinate and apply hit texture. 
+        }
+        Debug.Log("Pew!");
+    }
+
+    void RenderTracer() {
+        line.SetPositions(lineVertices);
     }
 
     IEnumerator Timer(float time) {
